7e8d3509e93997d0221de96aab387c4a34cd9005
dejuliansr <demetriojulian99@gmail.com>
2023-10-09 15:23:02 +0700

    berhasil-display-list-devicee

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,19 +4,16 @@
     <Feature />
 
     <Maps />
-
-    <coba />
 </template>
 
 <script>
 import Hero from "../sections/Hero.vue"
 import Maps from "../sections/Maps.vue"
 import Feature from "../sections/Feature.vue"
-import coba from "../sections/coba.vue"
 
 export default {
     components: {
-        Hero,Maps,Feature,coba
+        Hero,Maps,Feature
     }
 }
 </script>
